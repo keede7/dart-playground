6643861ef6ad545f10c724ab82f74f56a228c900
keede <wesaq11@gmail.com>
2023-10-28 17:32:03 +0900

    init Dart Playground

---
+++ dart_start_application/bin/dart_start_application.dart
@@ -0,0 +1,5 @@
+import 'package:dart_start_application/dart_start_application.dart' as dart_start_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_start_application.calculate()}!');
+}
