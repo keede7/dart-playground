f0fdada6659abe8eb8bb828e0ceb8c060f203bec
keede <wesaq11@gmail.com>
2023-11-11 21:55:59 +0900

    feat: condition Practice

---
+++ dart_start_application/bin/condition_loop_playground.dart
@@ -0,0 +1,22 @@
+
+void main() {
+
+  condition();
+}
+
+void condition() {
+
+  final int number = 30;
+
+  if(number == 30) {
+    print('true');
+  } else {
+    print('false');
+  }
+
+  if(number % 2 != 0) {
+    print('홀수');
+  } else {
+    print('짝수');
+  }
+}
